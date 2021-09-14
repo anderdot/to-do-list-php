f3dcb69001aef803edd8584f2522201d603b8e83
Anderson <62722855+anderdot@users.noreply.github.com>
2021-09-14 04:37:16 -0300

    Front base

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
+
+    <title>Meus afazeres</title>
+</head>
+
+<body>
+    <header>
+        <span class="material-icons">
+            <img src="img/outline_assignment_turned_in_white_24dp.png" width="64px" height="64">
+        </span>
+        <p class="title"> Meus afazeres</p>
+    </header>
+
+    <form class="form">
+        <div class="form-row">
+            <div class="form-group">
+                <input type="task" class="form-control" id="inputTask" placeholder="Sua tarefa">
+            </div>
+            <div class="button">
+                <button type="submit" class="btn btn-primary">Criar</button>
+            </div>
+        </div>
+        <div class="alert alert-primary" role="alert">
+            Um simples alerta primary. Olha só!
+        </div>
+        <div class="alert alert-secondary" role="alert">
+            Um simples alerta secondary. Olha só!
+        </div>
+        <div class="alert alert-success" role="alert">
+            Um simples alerta success. Olha só!
+        </div>
+        <div class="alert alert-danger" role="alert">
+            Um simples alerta danger. Olha só!
+        </div>
+        <div class="alert alert-warning" role="alert">
+            Um simples alerta warning. Olha só!
+        </div>
+        <div class="alert alert-info" role="alert">
+            Um simples alerta info. Olha só!
+        </div>
+        <div class="alert alert-dark" role="alert">
+            Um simples alerta dark. Olha só!
+        </div>
+    </form>
+</body>
+
+</html>
